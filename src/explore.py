692b1dec6701cedd2109a22660ac953a131c9f71
joshkeating <josh.keating7@gmail.com>
2018-11-03 17:50:29 -0400

    grabbed host data

--- src/explore.py
+++ src/explore.py
@@ -3,14 +3,12 @@ import pandas as pd
 import numpy as np
 
 
-def load_host_data():
-    file_path = '../data/listings.csv'
+def load_host_data(file_path):
     return pd.read_csv(file_path)
 
 
+full_df = load_host_data('./data/listings.csv')
 
-full_df = load_host_data()
+host_data = full_df[['host_id', 'host_url', 'host_name', 'host_location', 'host_about']]
 
-print(list(full_df))
-
-# host_data = full_df['host_id':'host_abput']
+print(host_data.head())
