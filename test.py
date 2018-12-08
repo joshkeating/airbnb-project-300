1849b6e6b30adb8c2235ff5c4e16760c87b8c141
joshkeating <josh.keating7@gmail.com>
2018-12-07 21:46:19 -0800

    made a lot of changes, updated analysis with keywords

--- test.py
+++ test.py
@@ -1,3 +1,84 @@
 
-
 import json
+from collections import defaultdict
+import pandas as pd
+
+
+
+# corpus = []
+
+# for elem in data1:
+#     keyPhrases = elem.get("keyPhrases")
+#     corpus.extend(keyPhrases)
+
+# for elem in data2:
+#     keyPhrases = elem.get("keyPhrases")
+#     corpus.extend(keyPhrases)
+
+# for elem in data3:
+#     keyPhrases = elem.get("keyPhrases")
+#     corpus.extend(keyPhrases)
+
+# # convert to lower case
+# corpus = [item.lower() for item in corpus]
+
+# # send to dict
+# uniq_counts = defaultdict(int)
+# for elem in corpus:
+#     uniq_counts[elem] += 1
+
+# data = pd.DataFrame.from_dict(data=uniq_counts, orient='index')
+
+# data.to_csv("./all_counts.csv")
+
+
+def load(file):
+    param =  "./" + file + ".json"
+    with open(param) as data:
+        return json.load(data).get("documents")
+
+data1 = load("keywords1")
+data2 = load("keywords2")
+data3 = load("keywords3")
+
+words = ['years', 'home', 'world', 'city', 'love', 'new people', ' university of washington', 'wonderful city', 'life motto', 'interior designer']
+
+output = {}
+
+
+for elem in data1:
+
+    cur_id = elem.get("id")
+
+    cur_keywords = elem.get("keyPhrases")
+
+    intersection = len(list(set(words).intersection(cur_keywords)))
+
+    output[cur_id] = intersection
+
+
+for elem in data2:
+    
+    cur_id = elem.get("id")
+
+    cur_keywords = elem.get("keyPhrases")
+
+    intersection = len(list(set(words).intersection(cur_keywords)))
+
+    output[cur_id] = intersection
+
+for elem in data3:
+
+    cur_id = elem.get("id")
+
+    cur_keywords = elem.get("keyPhrases")
+
+    intersection = len(list(set(words).intersection(cur_keywords)))
+
+    output[cur_id] = intersection
+
+
+data = pd.DataFrame.from_dict(data=output, orient='index')
+
+
+data.to_csv("./output.csv")
